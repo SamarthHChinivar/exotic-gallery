a479aa15975adb6e65859ad2cab884e367fd73bc
Samarth H Chinivar <samarthharish321@gmail.com>
2023-04-17 00:20:23 +0530

    supabase email db added

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,26 @@
+import { createClient } from '@supabase/supabase-js';
 import React, { useState } from 'react';
 
-const Contact = () => {
+export async function getServerSideProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  )
+
+  const { data: emailData, error: emailError } = await supabaseAdmin
+    .from('email')
+    .select('*')
+    .order('id');
+
+  return {
+    props: {
+      emailData, 
+      emailError,
+    },
+  }
+}
+
+const Contact = ({ emailData, emailError }) => {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -13,6 +33,23 @@ const Contact = () => {
     formData.append('file', file);
   };
 
+  const handleSubscribe = async (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+  
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+    const supabase = createClient(supabaseUrl, supabaseKey);
+  
+    const { error } = await supabase.from('email').insert({ email });
+  
+    if (error) {
+      console.log(error);
+    } else {
+      alert('Email added to database!');
+    }
+  };
+
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
         <h1 className='text-2xl font-bold text-center pt-16 pb-8'>Enter details to contribute:</h1>     
@@ -23,10 +60,10 @@ const Contact = () => {
             <button className='border shadow-lg p-3 w-full mt-3.5' type="submit">Submit</button>
         </form>
 
-        <h1 className='text-2xl font-bold text-center pt-16 pb-4'>Subscibe to E-mail updates:</h1>
-        <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
-            <input className='border shadow-lg w-full p-3' type="email" placeholder='Email' required />
-            <button className='border shadow-lg p-3 w-full mt-3' type="submit">Submit</button>
+        <h1 className='text-2xl font-bold text-center pt-16 pb-4'>Subscribe to E-mail updates:</h1>
+        <form className='max-w-[600px] m-auto' onSubmit={handleSubscribe}>
+            <input className='border shadow-lg w-full p-3' type="email" placeholder='Email'name="email" required />
+            <button className='border shadow-lg p-3 w-full mt-3' type="submit">Subscirbe</button>
         </form>
     </div>
   );
